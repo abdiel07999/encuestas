80527a8a5fb1f73021bc2ce5cb5086e277c51b35
abdiel07999 <110258192+abdiel07999@users.noreply.github.com>
2023-07-04 06:47:11 -0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,26 +1,27 @@
-CREATE DATABASE encuestas;
+CREATE DATABASE encuesta;
 
-USE encuestas;
+use encuesta;
 
 CREATE TABLE usuarios (
-    id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-    nombre_usuario VARCHAR(255),
-    contraseña VARCHAR(255),
-    correo_electronico VARCHAR(255)
+id_usuario int(11) primary key auto_increment,
+nombre_usuario varchar(255) not null,
+clave varchar(255) not null,
+correo_electronico varchar(255) not null,
+admin boolean
 );
 
-CREATE TABLE test_1(
-    id_test INT(2) AUTO_INCREMENT PRIMARY KEY, 
-    id_usuario INT(2) NOT NULL,
-    resultado_final VARCHAR(35) NOT NULL,
-    puntos INT(2) NOT NULL,
-    visual INT(4) NOT NULL,
-    cinestisico INT(4) NOT NULL,
-    auditivo INT(4) NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+CREATE TABLE test_1 (
+id_test int(11) primary key auto_increment,
+id_usuario int(11) not null,
+resultado_final varchar(35) default 'Desconocido',
+puntos int(11) not null,
+visual int(5) default 0,
+auditivo int(5) default 0,
+cinestesico int(5) default 0,
+foreign key (id_usuario) references usuarios(id_usuario)
+on delete cascade
+on update cascade
 );
 
 CREATE VIEW users_view_complete AS
-SELECT * FROM usuarios INNER JOIN test_1 ON usuarios.id_usuario = test_1.id_usuario;
+SELECT usuarios.id_usuario, usuarios.nombre_usuario, usuarios.clave, usuarios.correo_electronico, usuarios.admin, test_1.resultado_final FROM usuarios INNER JOIN test_1 ON usuarios.id_usuario = test_1.id_usuario;
